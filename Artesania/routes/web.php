8129a3dfebb281faabb377fe4ca9c202ba69a922
LuisaSandoval <72033699+LuisaSandoval@users.noreply.github.com>
2021-03-24 19:57:34 -0700

    Add files via upload

---
+++ Artesania/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/producto/{id}', function ($id) {
+    return view('verproducto')->with('id',$id);
+});
+Route::get('/contacto', function(){
+    $contacto='Maria Jaquelin';
+    $valores=10;
+    $color="#ccc";
+    return view('contacto')->with('nombre',$contacto)
+    ->with('fondo',$color)
+    ->with('valores',$valores);
+});
