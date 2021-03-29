d4260a248518f36f905d4ec14e189c8cf959b7d0
LuisaSandoval <72033699+LuisaSandoval@users.noreply.github.com>
2021-03-28 22:15:58 -0700

    Create web.php

---
+++ Artesanía/rutas/web.php
@@ -0,0 +1,14 @@
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/producto/{id}', function ($id) {
+    return view('verproducto')->with('id',$id);
+});
+Route::get('/contacto', function(){
+    $contacto='Maria Jaquelin';
+    $valores=10;
+    $color="#ccc";
+    return view('contacto')->with('nombre',$contacto)
+    ->with('fondo',$color)
+    ->with('valores',$valores);
+});
