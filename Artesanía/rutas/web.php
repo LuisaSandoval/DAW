d7c23e5d98d191c34b6c1060de47c74b9525cec9
LuisaSandoval <72033699+LuisaSandoval@users.noreply.github.com>
2021-03-28 22:08:27 -0700

    Update web.php

--- Artesanía/rutas/web.php
+++ Artesanía/rutas/web.php
@@ -20,7 +20,7 @@ Route::get('/producto/{id}', function ($id) {
     return view('verproducto')->with('id',$id);
 });
 Route::get('/contacto', function(){
-    $contacto='Maria Jaquelin';
+    $contacto='Luisa Fernanda';
     $valores=10;
     $color="#ccc";
     return view('contacto')->with('nombre',$contacto)
